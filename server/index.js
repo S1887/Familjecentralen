a443fff8742cb6beac85753ab5e605e2105c8d63
Svante A. Trendahl <svante.ortendahl@lidkoping.se>
2025-12-14 10:59:54 +0100

    Add request logging and loading indicator

--- server/index.js
+++ server/index.js
@@ -16,6 +16,12 @@ const PORT = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Request logging middleware
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // En enkel databas för att spara vem som gör vad
 
 // Hjälpfunktion för att läsa/skriva databas
